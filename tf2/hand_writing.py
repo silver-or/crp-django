0933b6bfb4331ae277f6c5b33c35fa61818bce1d
silver-or <laura817@naver.com>
2022-06-28 15:51:20 +0900

    2022-06-28 15:50 서브클래스 기법

--- tf2/hand_writing.py
+++ tf2/hand_writing.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from keras import datasets
+from keras import datasets, layers, models
 from tensorflow import keras  # 이미지 처리
-
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
 class Solution:
@@ -14,17 +15,37 @@ class Solution:
         test_images = test_images.reshape((10000, 28, 28, 1))
 
         # 픽셀 값을 0~1 사이로 정규화합니다.
-        self. train_images, self.test_images = train_images / 255.0, test_images / 255.0
+        self.train_images, self.test_images = train_images / 255.0, test_images / 255.0
+
+        self.layers = layers
+        self.model = models.Sequential()
+        self.train_labels = train_labels
 
     def solution(self):
-        models = keras.models
-        layers = keras.layers
-        model = models.Sequential()
+        self.modeling()
+        self.training()
+
+    def modeling(self):
+        model = self.model
+        layers = self.layers
         model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
         model.add(layers.MaxPooling2D((2, 2)))
         model.add(layers.Conv2D(64, (3, 3), activation='relu'))
         model.add(layers.MaxPooling2D((2, 2)))
         model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        model.add(layers.Flatten())
+        model.add(layers.Dense(64, activation='relu'))
+        model.add(layers.Dense(10, activation='softmax'))
+        model.summary()
+
+    def training(self):
+        model = self.model
+        model.compile(optimizer='adam',
+                      loss='sparse_categorical_crossentropy',
+                      metrics=['accuracy'])
+
+        model.fit(self.train_images, self.train_labels, epochs=5)
 
 
-
+if __name__ == '__main__':
+    Solution().solution()
